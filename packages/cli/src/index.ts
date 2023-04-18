ebaa79a1795a6359eea87e681bc3040fe0cb06b2
Thach Nguyen <thachnn80@gmail.com>
2023-04-18 19:08:55 +0700

    Patch the official `react-native-cli` v10.1.3 to bundle `react-native-init` CLI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,16 +1,13 @@
 import chalk from 'chalk';
-import childProcess from 'child_process';
 import {Command as CommanderCommand} from 'commander';
-import path from 'path';
 import {Command, Config} from '@react-native-community/cli-types';
-import {logger, CLIError} from '@react-native-community/cli-tools';
-import {detachedCommands, projectCommands} from './commands';
-import loadConfig from '@react-native-community/cli-config';
-const pkgJson = require('../package.json');
+import {logger} from '@react-native-community/cli-tools';
+import init from './commands/init';
+const detachedCommands = [init];
+const pkgVersion = require('../package.json').version;
 
 const program = new CommanderCommand()
-  .usage('[command] [options]')
-  .version(pkgJson.version, '-v', 'Output the current version')
+  .version(pkgVersion, '-v', 'Output the current version')
   .option('--verbose', 'Increase logging verbosity');
 
 const handleError = (err: Error) => {
@@ -72,7 +69,6 @@ function attachCommand<IsDetached extends boolean>(
   ...rest: IsDetached extends false ? [Config] : []
 ): void {
   const cmd = program
-    .command(command.name)
     .action(async function handleAction(
       this: CommanderCommand,
       ...args: string[]
@@ -91,6 +87,11 @@ function attachCommand<IsDetached extends boolean>(
       }
     });
 
+  const parts = command.name.match(/([^ ]+) *(.*)/);
+  if (parts) {
+    cmd.name(`react-native-${parts[1]}`);
+    if (parts[2]) cmd.arguments(parts[2]);
+  }
   if (command.description) {
     cmd.description(command.description);
   }
@@ -120,66 +121,17 @@ async function run() {
 async function setupAndRun() {
   // Commander is not available yet
 
-  // when we run `config`, we don't want to output anything to the console. We
-  // expect it to return valid JSON
-  if (process.argv.includes('config')) {
-    logger.disable();
-  }
 
   logger.setVerbose(process.argv.includes('--verbose'));
 
-  // We only have a setup script for UNIX envs currently
-  if (process.platform !== 'win32') {
-    const scriptName = 'setup_env.sh';
-    const absolutePath = path.join(__dirname, '..', scriptName);
-
-    try {
-      childProcess.execFileSync(absolutePath, {stdio: 'pipe'});
-    } catch (error) {
-      logger.warn(
-        `Failed to run environment setup script "${scriptName}"\n\n${chalk.red(
-          error,
-        )}`,
-      );
-      logger.info(
-        `React Native CLI will continue to run if your local environment matches what React Native expects. If it does fail, check out "${absolutePath}" and adjust your environment to match it.`,
-      );
-    }
-  }
 
   for (const command of detachedCommands) {
     attachCommand(command);
   }
 
-  try {
-    const config = loadConfig();
-
-    logger.enable();
-
-    for (const command of [...projectCommands, ...config.commands]) {
-      attachCommand(command, config);
-    }
-  } catch (error) {
-    /**
-     * When there is no `package.json` found, the CLI will enter `detached` mode and a subset
-     * of commands will be available. That's why we don't throw on such kind of error.
-     */
-    if (error.message.includes("We couldn't find a package.json")) {
-      logger.debug(error.message);
-      logger.debug(
-        'Failed to load configuration of your project. Only a subset of commands will be available.',
-      );
-    } else {
-      throw new CLIError(
-        'Failed to load configuration of your project.',
-        error,
-      );
-    }
-  }
 
   program.parse(process.argv);
 }
 
-const bin = require.resolve('./bin');
 
-export {run, bin, loadConfig};
+export {run};
